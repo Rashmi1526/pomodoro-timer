6e050d12a3bc66a3114c9abf851f1b6e7b1d3c18
Rashmi1526 <rashmi.19b101039@abes.ac.in>
2022-06-15 23:33:19 +0530

    Updates

---
+++ precache-manifest.d2f627775cc6f7a84b79669ebc2ed75a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a3d2fe03e1b375815f4e5b56381ca2",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "e658023a6f9924240bfc",
+    "url": "/pomodoro-timer/static/css/main.57691233.chunk.css"
+  },
+  {
+    "revision": "b27498396b6d9937e869",
+    "url": "/pomodoro-timer/static/js/2.9cd6da6d.chunk.js"
+  },
+  {
+    "revision": "e658023a6f9924240bfc",
+    "url": "/pomodoro-timer/static/js/main.ad5dd664.chunk.js"
+  },
+  {
+    "revision": "8d84c672a99e0f0ece83",
+    "url": "/pomodoro-timer/static/js/runtime~main.2a0df6ba.js"
+  }
+]);
